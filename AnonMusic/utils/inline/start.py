13b109583bca99efea60c20e14727deb2ac27e95
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-08-11 15:22:11 +0530

    Update start.py

--- AnonMusic/utils/inline/start.py
+++ AnonMusic/utils/inline/start.py
@@ -24,12 +24,11 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHANNEL),
         ],
-        [InlineKeyboardButton(text=_["S_B_6"], web_app=WebAppInfo(url=config.WEBSITE))],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     
     return buttons
